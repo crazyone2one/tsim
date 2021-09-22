5b99034b8fff0f7929b2620b6bc4f3b4f67087bb
jingll.li <the2ndindec@hotmail.com>
2021-09-22 10:14:15 +0800

    :tada: 创建项目

---
+++ src/main/java/cn/master/tsim/TsimApplication.java
@@ -0,0 +1,16 @@
+package cn.master.tsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jingll
+ */
+@SpringBootApplication
+public class TsimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/master/tsim/TsimApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.master.tsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
