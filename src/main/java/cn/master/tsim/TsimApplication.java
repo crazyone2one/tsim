ec2d93c1462b794c3995c8ad05cdd9ae139c0c82
jingll.li <the2ndindec@hotmail.com>
2021-09-26 14:56:36 +0800

    :sparkles: MybatisPlus

--- src/main/java/cn/master/tsim/TsimApplication.java
+++ src/main/java/cn/master/tsim/TsimApplication.java
@@ -1,5 +1,6 @@
 package cn.master.tsim;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author jingll
  */
 @SpringBootApplication
+@MapperScan("cn.master.tsim.mapper")
 public class TsimApplication {
 
     public static void main(String[] args) {
