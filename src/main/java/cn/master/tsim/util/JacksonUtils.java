f98386338008d175a4727040f0d2f27d148572c9
jingll.li <the2ndindec@hotmail.com>
2021-10-13 16:01:27 +0800

    :zap: JacksonUtils

--- src/main/java/cn/master/tsim/util/JacksonUtils.java
+++ src/main/java/cn/master/tsim/util/JacksonUtils.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Created by 11's papa on 2021/09/23
@@ -15,6 +17,7 @@ import java.util.Map;
  */
 public class JacksonUtils {
     private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     static {
         OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
@@ -22,6 +25,7 @@ public class JacksonUtils {
         OBJECT_MAPPER.configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
         OBJECT_MAPPER.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
     }
+
     public static Map<String, Object> convertStringToJson(String source) {
         Map<String, Object> objectMap = new LinkedHashMap<>();
         try {
@@ -31,5 +35,49 @@ public class JacksonUtils {
             e.printStackTrace();
         }
         return objectMap;
+    }
+
+    /**
+     * 对象转json格式字符串
+     *
+     * @param object
+     * @return java.lang.String
+     */
+    public static String convertToString(Object object) {
+        String value = "";
+        try {
+            value = OBJECT_MAPPER.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    /**
+     * 字符串转自定义对象
+     *
+     * @param source
+     * @param clazz
+     * @return T
+     */
+    public static <T> T convertToClass(String source, Class<T> clazz) {
+        if (StringUtils.isBlank(source) || Objects.isNull(clazz)) {
+            return null;
+        }
+        try {
+            return OBJECT_MAPPER.readValue(source, clazz);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public <T> T jsonToObject(String responseJson, TypeReference<T> valueTypeRef) {
+        try {
+            return OBJECT_MAPPER.readValue(responseJson, valueTypeRef);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
