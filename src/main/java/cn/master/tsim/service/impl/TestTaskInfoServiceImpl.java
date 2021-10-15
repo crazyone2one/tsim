c13422bd35e1f28558897b5547763b1f27b99362
11's papa <the2ndindec@gmail.com>
2021-10-15 11:29:25 +0800

    :zap: 任务汇总模块查询功能

--- src/main/java/cn/master/tsim/service/impl/TestTaskInfoServiceImpl.java
+++ src/main/java/cn/master/tsim/service/impl/TestTaskInfoServiceImpl.java
@@ -1,22 +1,23 @@
 package cn.master.tsim.service.impl;
 
 import cn.master.tsim.entity.Project;
+import cn.master.tsim.entity.TestStory;
 import cn.master.tsim.entity.TestTaskInfo;
 import cn.master.tsim.entity.Tester;
 import cn.master.tsim.mapper.TestTaskInfoMapper;
-import cn.master.tsim.service.ProjectService;
-import cn.master.tsim.service.TestBugService;
-import cn.master.tsim.service.TestCaseService;
-import cn.master.tsim.service.TestTaskInfoService;
+import cn.master.tsim.service.*;
 import cn.master.tsim.util.JacksonUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -34,6 +35,8 @@ public class TestTaskInfoServiceImpl extends ServiceImpl<TestTaskInfoMapper, Tes
 
     @Autowired
     private TestBugService bugService;
+    @Autowired
+    private TestStoryService storyService;
 
     @Autowired
     public TestTaskInfoServiceImpl(ProjectService projectService, TestCaseService caseService) {
@@ -44,12 +47,22 @@ public class TestTaskInfoServiceImpl extends ServiceImpl<TestTaskInfoMapper, Tes
     @Override
     public IPage<TestTaskInfo> taskInfoPage(TestTaskInfo taskInfo, Integer pageCurrent, Integer pageSize) {
         QueryWrapper<TestTaskInfo> wrapper = new QueryWrapper<>();
+//        根据项目名称模糊查询
+        wrapper.lambda().inSql(StringUtils.isNotBlank(taskInfo.getProjectId()),
+                TestTaskInfo::getProjectId, "select id from project where project_name like '%" + taskInfo.getProjectId() + "%'");
+//        根据完成状态查询
+        wrapper.lambda().eq(StringUtils.isNotBlank(taskInfo.getFinishStatus()), TestTaskInfo::getFinishStatus, taskInfo.getFinishStatus());
+//        根据任务时间查询
+        wrapper.lambda().eq(StringUtils.isNotBlank(taskInfo.getIssueDate()), TestTaskInfo::getIssueDate, taskInfo.getIssueDate());
         final Page<TestTaskInfo> testTaskInfoPage = baseMapper.selectPage(
                 new Page<>(Objects.equals(pageCurrent, 0) ? 1 : pageCurrent, Objects.equals(pageSize, 0) ? 15 : pageSize),
                 wrapper);
-        testTaskInfoPage.getRecords().forEach(t->{
-            t.setSubBug(bugService.bugMapByProject(t.getProjectId(),"1"));
-            t.setFixBug(bugService.bugMapByProject(t.getProjectId(),"4"));
+        testTaskInfoPage.getRecords().forEach(t -> {
+            t.setSubBug(bugService.bugMapByProject(t.getProjectId(), "1"));
+            t.setFixBug(bugService.bugMapByProject(t.getProjectId(), "4"));
+            //            设置任务描述为需求内容
+            List<TestStory> storyList = storyService.listStoryByProjectAndWorkDate(t.getProjectId(), t.getIssueDate());
+            t.setSummaryDesc(CollectionUtils.isNotEmpty(storyList) ? storyList.get(0).getDescription() : "");
             t.setProjectId(projectService.getProjectById(t.getProjectId()).getProjectName());
         });
         return testTaskInfoPage;
