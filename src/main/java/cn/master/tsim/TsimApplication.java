c8b8174c84d32fd8b5bfac06fe85a4593600ffe1
11's papa <the2ndindec@gmail.com>
2021-11-26 13:11:00 +0800

    :zap: 文件保存功能优化

--- src/main/java/cn/master/tsim/TsimApplication.java
+++ src/main/java/cn/master/tsim/TsimApplication.java
@@ -1,14 +1,17 @@
 package cn.master.tsim;
 
+import cn.master.tsim.config.FileProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author jingll
  */
 @SpringBootApplication
 @MapperScan("cn.master.tsim.mapper")
+@EnableConfigurationProperties({FileProperties.class})
 public class TsimApplication {
 
     public static void main(String[] args) {
