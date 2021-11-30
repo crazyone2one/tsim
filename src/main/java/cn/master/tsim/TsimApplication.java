f67eed9750eb88bc5fa89a22f02f1b399742fd52
jingll.li <the2ndindec@hotmail.com>
2021-11-30 09:38:54 +0800

    :zap: 更新war生成方式

--- src/main/java/cn/master/tsim/TsimApplication.java
+++ src/main/java/cn/master/tsim/TsimApplication.java
@@ -4,7 +4,9 @@ import cn.master.tsim.config.FileProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author jingll
@@ -12,10 +14,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @MapperScan("cn.master.tsim.mapper")
 @EnableConfigurationProperties({FileProperties.class})
-public class TsimApplication {
+public class TsimApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TsimApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TsimApplication.class);
+    }
 }
