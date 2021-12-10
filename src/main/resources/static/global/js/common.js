a269349b502885f1bdb624acb48d971529b610cd
jingll.li <the2ndindec@hotmail.com>
2021-12-10 11:03:06 +0800

    :sparkles: 1. 更新折叠菜单的方法 2.增加手动关闭modal时清空表单内容的方法

--- src/main/resources/static/global/js/common.js
+++ src/main/resources/static/global/js/common.js
@@ -1,63 +1,11 @@
-(function ($) {
-    "use strict"; // Start of use strict
-
-    // Toggle the side navigation
-    $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
-        $("body").toggleClass("sidebar-toggled");
-        $(".sidebar").toggleClass("toggled");
-        if ($(".sidebar").hasClass("toggled")) {
-            $('.sidebar .collapse').collapse('hide');
-        }
-        ;
-    });
-
-    // Close any open menu accordions when window is resized below 768px
-    $(window).resize(function () {
-        if ($(window).width() < 768) {
-            $('.sidebar .collapse').collapse('hide');
-        }
-        ;
-
-        // Toggle the side navigation when window is resized below 480px
-        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-            $("body").addClass("sidebar-toggled");
-            $(".sidebar").addClass("toggled");
-            $('.sidebar .collapse').collapse('hide');
-        }
-        ;
-    });
-
-    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
-        if ($(window).width() > 768) {
-            var e0 = e.originalEvent,
-                delta = e0.wheelDelta || -e0.detail;
-            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-            e.preventDefault();
-        }
-    });
-
-    // Scroll to top button appear
-    $(document).on('scroll', function () {
-        var scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
-
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function (e) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutExpo');
-        e.preventDefault();
-    });
-
-})(jQuery); // End of use strict
-
+// Toggle the side navigation
+$("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+        $('.sidebar .collapse').collapse('hide');
+    }
+});
 /**
  * 使用bootstrap toast作为消息提示
  * @param code ajax请求后返回的状态码，用于决定toast的类型
@@ -149,3 +97,11 @@ function resetModal(modalId, formId) {
         console.log(" hidden >> reset modal completed");
     })
 }
+/*
+手动关闭modal弹框时清空已输入的内容
+1. 点击关闭、取消按钮
+2. 点击 x
+*/
+$(".modal").on('hide.bs.modal', function (e) {
+    $(".modal form")[0].reset();
+});
