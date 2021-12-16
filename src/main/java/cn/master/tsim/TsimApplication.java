97f3e7228bc2adce76df4176f87a2795a93ea489
jingll.li <the2ndindec@hotmail.com>
2021-12-16 10:57:29 +0800

    :zap: 使用spring-boot-starter-security

--- src/main/java/cn/master/tsim/TsimApplication.java
+++ src/main/java/cn/master/tsim/TsimApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author jingll
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 @MapperScan("cn.master.tsim.mapper")
 @EnableConfigurationProperties({FileProperties.class})
 public class TsimApplication extends SpringBootServletInitializer {
@@ -22,5 +24,10 @@ public class TsimApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TsimApplication.class);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
