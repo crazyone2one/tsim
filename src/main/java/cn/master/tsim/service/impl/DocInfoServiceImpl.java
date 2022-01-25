2eb222ca868d1df955ccc996b8462f9844772f7a
jingll.li <the2ndindec@hotmail.com>
2022-01-25 09:31:49 +0800

    :zap: Transactional

--- src/main/java/cn/master/tsim/service/impl/DocInfoServiceImpl.java
+++ src/main/java/cn/master/tsim/service/impl/DocInfoServiceImpl.java
@@ -5,6 +5,7 @@ import cn.master.tsim.mapper.DocInfoMapper;
 import cn.master.tsim.service.DocInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -22,6 +23,7 @@ import java.util.Map;
 public class DocInfoServiceImpl extends ServiceImpl<DocInfoMapper, DocInfo> implements DocInfoService {
 
     @Override
+    @Transactional(rollbackFor = {RuntimeException.class, Exception.class})
     public DocInfo saveDocInfo(HttpServletRequest request, Map<String, String> docInfo) {
         DocInfo build = DocInfo.builder().docName(docInfo.get("docName")).uuidName(docInfo.get("uuidName"))
                 .docFlag(docInfo.get("flag")).docPath(docInfo.get("docPath")).delFlag(0)
